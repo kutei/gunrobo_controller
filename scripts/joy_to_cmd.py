1180f68123fd8fed5f7e7e6bbab1bc9018430359
kutei <kutei@example.com>
2020-11-14 14:58:28 +0900

    joystickで操作できるようにした

---
+++ scripts/joy_to_cmd.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+class JoyToCmd:
+    def __init__(self):
+        self.cmd_vel = Twist()
+
+    def joy_callback(self, data):
+        rospy.loginfo("koko")
+        self.cmd_vel.linear.x = data.axes[3]
+        self.cmd_vel.angular.z = data.axes[2]
+
+    def start_converter(self):
+        rospy.init_node('joy_to_cmd')
+
+        rospy.Subscriber("/joy", Joy, self.joy_callback)
+        cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+
+        rater = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            cmd_pub.publish(self.cmd_vel)
+            rater.sleep()
+
+if __name__ == "__main__":
+    JoyToCmd().start_converter()
